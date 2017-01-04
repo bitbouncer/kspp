9b648080a452ebd938705b6e3c895f841e3c1f95
saka <svante.karlsson@csi.se>
2017-01-04 10:54:40 +0100

    inline added to template specializations

--- kspp/codecs/text_codec.h
+++ kspp/codecs/text_codec.h
@@ -26,48 +26,48 @@ namespace csi {
     }
   };
 
-  template<> size_t text_codec::encode(const std::string& src, std::ostream& dst) {
+  template<> inline size_t text_codec::encode(const std::string& src, std::ostream& dst) {
     dst << src;
     return src.size();
   }
 
-  template<> size_t text_codec::decode(std::istream& src, std::string& dst) {
+  template<> inline size_t text_codec::decode(std::istream& src, std::string& dst) {
     dst.clear();
     std::getline(src, dst);
     return dst.size();
   }
 
-  template<> size_t text_codec::encode(const bool& src, std::ostream& dst) {
+  template<> inline size_t text_codec::encode(const bool& src, std::ostream& dst) {
     dst << src ? "true" : "false";
     return src ? 4 : 5;
   }
 
-  template<> size_t text_codec::decode(std::istream& src, bool& dst) {
+  template<> inline size_t text_codec::decode(std::istream& src, bool& dst) {
     std::string s;
     std::getline(src, s);
     dst = (s == "true") ? true : false;
     return s.size();
   }
 
-  template<> size_t text_codec::encode(const int& src, std::ostream& dst) {
+  template<> inline size_t text_codec::encode(const int& src, std::ostream& dst) {
     auto s = std::to_string(src);
     dst << s;
     return s.size();
   }
    
-  template<> size_t text_codec::encode(const int64_t& src, std::ostream& dst) {
+  template<> inline size_t text_codec::encode(const int64_t& src, std::ostream& dst) {
     auto s = std::to_string(src);
     dst << s;
     return s.size();
   }
 
-  template<> size_t text_codec::encode(const uint64_t& src, std::ostream& dst) {
+  template<> inline size_t text_codec::encode(const uint64_t& src, std::ostream& dst) {
     auto s = std::to_string(src);
     dst << s;
     return s.size();
   }
      
-  template<> size_t text_codec::decode(std::istream& src, int& dst) {
+  template<> inline size_t text_codec::decode(std::istream& src, int& dst) {
     std::string s;
     std::getline(src, s);
     dst = std::atoi(s.c_str());
