f56c62e77ddd99d792aef96603d2d18928543356
saka <svante.karlsson@csi.se>
2017-05-19 12:03:23 +0200

    adds support for partion_topology

--- kspp/metrics/influx_metrics_reporter.cpp
+++ kspp/metrics/influx_metrics_reporter.cpp
@@ -21,17 +21,15 @@ static std::string hostname() {
 #endif
 }
 
-influx_metrics_reporter::influx_metrics_reporter(kspp::topology_builder& builder, std::string topic, std::vector<std::shared_ptr<topic_topology>> topologys, std::string prefix, std::string tags)
+influx_metrics_reporter::influx_metrics_reporter(kspp::topology_builder& builder, std::string topic, std::string prefix, std::string tags)
   : _run(true)
   , _topic(topic)
-  , _reported_topologys(topologys)
   , _prefix(prefix)
   , _tags(tags)
   , _hostname(hostname()) {
   _metrics_topology = builder.create_topology();
   _sink = _metrics_topology->create_sink<kspp::kafka_topic_sink<std::string, std::string, kspp::text_serdes>>(_topic);
-  for (auto  i : _reported_topologys)
-    i->init_metrics();
+
   _thread = std::make_shared<std::thread>([this]() {
     std::string base_string = _prefix + (_tags.size() ? "," + _tags : "");
     int64_t next_time_to_send = kspp::milliseconds_since_epoch() + 10 * 1000;
@@ -44,8 +42,8 @@ influx_metrics_reporter::influx_metrics_reporter(kspp::topology_builder& builder
       if (next_time_to_send <= kspp::milliseconds_since_epoch()) {
         uint64_t measurement_time = milliseconds_since_epoch();
         std::string measurement_time_str = std::to_string(milliseconds_since_epoch()) + "000000";
-        
-        for (auto i : _reported_topologys) {
+
+        for (auto i : _reported_p_topologys) {
           i->for_each_metrics([this, &base_string, &measurement_time_str, &measurement_time](kspp::metric& m) {
             std::string measurement_tags = m.tags();
             // influxdb line format
@@ -53,6 +51,16 @@ influx_metrics_reporter::influx_metrics_reporter(kspp::topology_builder& builder
             _sink->produce(_hostname, value, measurement_time);
           });
         }
+
+        for (auto i : _reported_t_topologys) {
+          i->for_each_metrics([this, &base_string, &measurement_time_str, &measurement_time](kspp::metric& m) {
+            std::string measurement_tags = m.tags();
+            // influxdb line format
+            std::string value = base_string + (measurement_tags.size() ? "," + measurement_tags : "") + " " + m.name() + "=" + std::to_string(m.value()) + "i " + measurement_time_str;
+            _sink->produce(_hostname, value, measurement_time);
+          });
+        }
+
         //schedule nex reporting event
         next_time_to_send += 10000;
         // if we are reaaly out of sync lets sleep at least 10 more seconds
@@ -69,4 +77,38 @@ influx_metrics_reporter::~influx_metrics_reporter() {
   _thread->join();
 }
 
+void influx_metrics_reporter::add_metrics(std::shared_ptr<topic_topology> p) {
+  p->init_metrics();
+  // maybe we need a mutex here... TBD
+  _reported_t_topologys.push_back(p); 
+}
+
+void influx_metrics_reporter::add_metrics(std::shared_ptr<partition_topology> p) {
+  p->init_metrics();
+  // maybe we need a mutex here... TBD
+  _reported_p_topologys.push_back(p);
+}
+
+
+std::shared_ptr<influx_metrics_reporter> operator<<(std::shared_ptr<influx_metrics_reporter> reporter, std::shared_ptr<topic_topology> t) {
+  reporter->add_metrics(t);
+  return reporter;
+}
+
+std::shared_ptr<influx_metrics_reporter> operator<<(std::shared_ptr<influx_metrics_reporter> reporter, std::shared_ptr<partition_topology> t) {
+  reporter->add_metrics(t);
+  return reporter;
+}
+
+std::shared_ptr<influx_metrics_reporter> operator<<(std::shared_ptr<influx_metrics_reporter> reporter, std::vector<std::shared_ptr<topic_topology>> v) {
+  for (auto i : v)
+    reporter->add_metrics(i);
+  return reporter;
+}
+
+std::shared_ptr<influx_metrics_reporter> operator<<(std::shared_ptr<influx_metrics_reporter> reporter, std::vector<std::shared_ptr<partition_topology>> v) {
+  for (auto i : v)
+    reporter->add_metrics(i);
+  return reporter;
+}
 };
--- kspp/metrics/influx_metrics_reporter.h
+++ kspp/metrics/influx_metrics_reporter.h
@@ -2,6 +2,7 @@
 #include <memory>
 #include <thread>
 #include <kspp/kspp.h>
+#include <kspp/topology_builder.h>
 #include <kspp/impl/serdes/text_serdes.h>
 #include <kspp/sinks/kafka_sink.h>
 #pragma once
@@ -10,13 +11,17 @@ namespace kspp {
 class influx_metrics_reporter
 {
   public:
-  influx_metrics_reporter(kspp::topology_builder& builder, std::string topic, std::vector<std::shared_ptr<topic_topology>>, std::string prefix, std::string tags);
+  influx_metrics_reporter(kspp::topology_builder& builder, std::string topic, std::string prefix, std::string tags);
   ~influx_metrics_reporter();
+
+  void add_metrics(std::shared_ptr<topic_topology> p);
+  void add_metrics(std::shared_ptr<partition_topology> p);
 
   private:
   bool                                                                                 _run;
   const std::string                                                                    _topic;
-  std::vector<std::shared_ptr<topic_topology>>                                         _reported_topologys;
+  std::vector<std::shared_ptr<topic_topology>>                                         _reported_t_topologys;
+  std::vector<std::shared_ptr<partition_topology>>                                     _reported_p_topologys;
   std::shared_ptr<topic_topology>                                                      _metrics_topology;
   std::shared_ptr<kspp::kafka_topic_sink<std::string, std::string, kspp::text_serdes>> _sink;
   const std::string                                                                    _prefix;
@@ -24,4 +29,9 @@ class influx_metrics_reporter
   const std::string                                                                    _hostname;
   std::shared_ptr<std::thread>                                                         _thread;
 };
+
+std::shared_ptr<influx_metrics_reporter> operator<<(std::shared_ptr<influx_metrics_reporter> reporter, std::shared_ptr<topic_topology> t);
+std::shared_ptr<influx_metrics_reporter> operator<<(std::shared_ptr<influx_metrics_reporter> reporter, std::shared_ptr<partition_topology> t);
+std::shared_ptr<influx_metrics_reporter> operator<<(std::shared_ptr<influx_metrics_reporter> reporter, std::vector<std::shared_ptr<topic_topology>> v);
+std::shared_ptr<influx_metrics_reporter> operator<<(std::shared_ptr<influx_metrics_reporter> reporter, std::vector<std::shared_ptr<partition_topology>> v);
 };
