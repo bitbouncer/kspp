e3962e1fbc13646b6b7721f7f0fc2c83a511a4d9
saka <svante.karlsson@csi.se>
2017-05-29 16:08:17 +0200

    update

--- kspp/processors/transform.h
+++ kspp/processors/transform.h
@@ -79,5 +79,93 @@ namespace kspp {
     std::deque<std::shared_ptr<ktransaction<K, SV>>> _queue;
     metric_lag                                  _lag;
   };
+
+
+  template<class K, class V>
+  class transform : public partition_source<K, V>
+  {
+    public:
+    typedef std::function<void(std::shared_ptr<ktransaction<K, V>> record, transform* self)> extractor; // maybe better to pass this and send() directrly
+
+    transform(topology_base& topology, std::shared_ptr<partition_source<K, V>> source, extractor f)
+      : partition_source<K, V>(source.get(), source->partition())
+      , _source(source)
+      , _extractor(f) {
+      _source->add_sink([this](auto r) {
+        _queue.push_back(r);
+      });
+      this->add_metric(&_lag);
+    }
+
+    ~transform() {
+      close();
+    }
+
+    std::string name() const {
+      return _source->name() + "-transform";
+    }
+
+    virtual std::string processor_name() const {
+      return "transform";
+    }
+
+    virtual void start() {
+      _source->start();
+    }
+
+    virtual void start(int64_t offset) {
+      _source->start(offset);
+    }
+
+    virtual void close() {
+      _source->close();
+    }
+
+    virtual bool process_one(int64_t tick) {
+      _source->process_one(tick);
+      bool processed = (_queue.size() > 0);
+      while (_queue.size()) {
+        auto trans = _queue.front();
+        _lag.add_event_time(tick, trans->event_time());
+        _queue.pop_front();
+        _currrent_id = trans->id(); // we capture this to have it in push_back callback
+        _extractor(trans, this);
+        _currrent_id.reset(); // must be freed otherwise we continue to hold the last transaction
+      }
+      return processed;
+    }
+
+    /*
+    void push_back(std::shared_ptr<ktransaction<K, V>> r) {
+      this->send_to_sinks(r);
+    }
+    */
+
+    /**
+    * use from from extractor callback
+    */
+    inline void push_back(std::shared_ptr<krecord<K, V>> record) {
+      this->send_to_sinks(std::make_shared<ktransaction<K, V>>(record, _currrent_id));
+    }
+
+    virtual void commit(bool flush) {
+      _source->commit(flush);
+    }
+
+    virtual bool eof() const {
+      return _source->eof() && (_queue.size() == 0);
+    }
+
+    virtual size_t queue_len() {
+      return _queue.size();
+    }
+
+    private:
+    std::shared_ptr<partition_source<K, V>>           _source;
+    extractor                                         _extractor;
+    std::shared_ptr<commit_chain::autocommit_marker>  _currrent_id; // used to briefly hold the commit open during process one
+    std::deque<std::shared_ptr<ktransaction<K, V>>>   _queue;
+    metric_lag                                        _lag;
+  };
 }
 
