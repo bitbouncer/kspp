6eb15ca198f594d5a9ddc3717572e7f91eb71200
skarlsson <svante.karlsson@csi.se>
2017-05-30 22:41:19 +0200

    simplified code

--- kspp/sinks/stream_sink.h
+++ kspp/sinks/stream_sink.h
@@ -9,6 +9,32 @@
 namespace kspp {
 
   template<class K, class V>
+  void print(std::shared_ptr<kevent<K, V>> ev, std::ostream& _os, kspp::text_serdes* _codec) {
+    _os << "ts: " << ev->event_time() << "  ";
+    _codec->encode(ev->record()->key, _os);
+    _os << ":";
+    if (ev->record()->value)
+      _codec->encode(*ev->record()->value, _os);
+    else
+      _os << "<nullptr>";
+    _os << std::endl;
+  }
+
+  template<class V>
+  void print(std::shared_ptr<kevent<void, V>> ev, std::ostream& _os, kspp::text_serdes* _codec) {
+    _os << "ts: " << ev->event_time() << "  ";
+    _codec->encode(*ev->record()->value, _os);
+    _os << std::endl;
+  }
+
+  template<class K>
+  void print(std::shared_ptr<kevent<K, void>> ev, std::ostream& _os, kspp::text_serdes* _codec) {
+    _os << "ts: " << ev->event_time() << "  ";
+    _codec->encode(ev->record()->key, _os);
+    _os << std::endl;
+  }
+
+  template<class K, class V>
   class stream_sink : public partition_sink<K, V>
   {
   public:
@@ -45,132 +71,181 @@ namespace kspp {
       size_t count = 0;
       while (this->_queue.size()) {
         auto ev = this->_queue.front();
-        handle_event(ev);
-        this->_queue.pop_front();
-      }
-      return (count > 0);
-    }
-
-  private:
-    void handle_event(std::shared_ptr<kevent<K, V>> ev) {
-      _os << "ts: " << ev->event_time() << "  ";
-      _codec->encode(ev->record()->key, _os);
-      _os << ":";
-      if (ev->record()->value)
-        _codec->encode(*ev->record()->value, _os);
-      else
-        _os << "<nullptr>";
-      _os << std::endl;
-    }
-
-    std::ostream&                      _os;
-    std::shared_ptr<kspp::text_serdes> _codec;
-  };
-
-  //<null, VALUE>
-  template<class V>
-  class stream_sink<void, V> : public partition_sink<void, V>
-  {
-  public:
-    stream_sink(topology_base& topology, std::shared_ptr<partition_source<void, V>> source, std::ostream* os)
-      : partition_sink<void, V>(source.partition())
-      , _os(*os)
-      , _codec(std::make_shared<kspp::text_serdes>()) {
-      source->add_sink(this);
-    }
-
-    virtual ~stream_sink() {
-      this->flush();;
-    }
-
-    virtual std::string name() const {
-      return "stream_sink";
-    }
-
-    virtual std::string processor_name() const {
-      return "stream_sink";
-    }
-
-    virtual bool process_one(int64_t tick) {
-      size_t count = 0;
-      while (this->_queue.size()) {
-        auto ev = this->_queue.front();
-        handle_event(ev);
+        print(ev, _os, _codec.get());
         this->_queue.pop_front();
       }
       return (count > 0);
     }
  
-    virtual size_t queue_len() {
-      return this->_queue.size();
-    }
-
-    virtual void commit(bool flush) {
-      // noop
-    }
-     
-  private:
-    void handle_event(std::shared_ptr<kevent<void, V>> ev) {
-      _os << "ts: " << ev->event_time() << "  ";
-      _codec->encode(*ev->record()->value, _os);
-      _os << std::endl;
-    }
-
     std::ostream&                      _os;
     std::shared_ptr<kspp::text_serdes> _codec;
   };
 
-  // <key, nullptr>
-  template<class K>
-  class stream_sink<K, void> : public partition_sink<K, void>
-  {
-  public:
-    stream_sink(topology_base& topology, std::shared_ptr<partition_source<K, void>> source, std::ostream* os)
-      : partition_sink<K, void>(source->partition())
-      , _os(*os)
-      , _codec(std::make_shared<kspp::text_serdes>()) {
-      source->add_sink(this);
-    }
 
-    virtual ~stream_sink() {
-      this->flush();;
-    }
 
-    virtual std::string name() const {
-      return "stream_sink";
-    }
+  //template<class K, class V>
+  //class stream_sink : public partition_sink<K, V>
+  //{
+  //public:
+  //  enum { MAX_KEY_SIZE = 1000 };
 
-    virtual std::string processor_name() const {
-      return "stream_sink";
-    }
+  //  stream_sink(topology_base& topology, std::shared_ptr<partition_source<K, V>> source, std::ostream* os)
+  //    : partition_sink<K, V>(source->partition())
+  //    , _os(*os)
+  //    , _codec(std::make_shared<kspp::text_serdes>()) {
+  //    source->add_sink(this);
+  //  }
 
-    virtual bool process_one(int64_t tick) {
-      size_t count = 0;
-      while (this->_queue.size()) {
-        auto ev = this->_queue.front();
-        handle_event(ev);
-        this->_queue.pop_front();
-      }
-      return (count > 0);
-    }
+  //  virtual ~stream_sink() {
+  //    this->flush();;
+  //  }
 
-    virtual size_t queue_len() {
-      return this->_queue.size();
-    }
+  //  virtual std::string name() const {
+  //    return "stream_sink";
+  //  }
 
-    virtual void commit(bool flush) {
-      // noop
-    }
+  //  virtual std::string processor_name() const {
+  //    return "stream_sink";
+  //  }
 
-  private:
-    void handle_event(std::shared_ptr<kevent<K, void>> ev) {
-      _os << "ts: " << ev->event_time() << "  ";
-      _codec->encode(ev->record()->key, _os);
-      _os << std::endl;
-    }
+  //  virtual size_t queue_len() {
+  //    return this->_queue.size();
+  //  }
 
-    std::ostream&                      _os;
-    std::shared_ptr<kspp::text_serdes> _codec;
-  };
+  //  virtual void commit(bool flush) {
+  //    // noop
+  //  }
+
+  //  virtual bool process_one(int64_t tick) {
+  //    size_t count = 0;
+  //    while (this->_queue.size()) {
+  //      auto ev = this->_queue.front();
+  //      handle_event(ev);
+  //      this->_queue.pop_front();
+  //    }
+  //    return (count > 0);
+  //  }
+
+  //private:
+  //  void handle_event(std::shared_ptr<kevent<K, V>> ev) {
+  //    _os << "ts: " << ev->event_time() << "  ";
+  //    _codec->encode(ev->record()->key, _os);
+  //    _os << ":";
+  //    if (ev->record()->value)
+  //      _codec->encode(*ev->record()->value, _os);
+  //    else
+  //      _os << "<nullptr>";
+  //    _os << std::endl;
+  //  }
+
+  //  std::ostream&                      _os;
+  //  std::shared_ptr<kspp::text_serdes> _codec;
+  //};
+
+  ////<null, VALUE>
+  //template<class V>
+  //class stream_sink<void, V> : public partition_sink<void, V>
+  //{
+  //public:
+  //  stream_sink(topology_base& topology, std::shared_ptr<partition_source<void, V>> source, std::ostream* os)
+  //    : partition_sink<void, V>(source.partition())
+  //    , _os(*os)
+  //    , _codec(std::make_shared<kspp::text_serdes>()) {
+  //    source->add_sink(this);
+  //  }
+
+  //  virtual ~stream_sink() {
+  //    this->flush();;
+  //  }
+
+  //  virtual std::string name() const {
+  //    return "stream_sink";
+  //  }
+
+  //  virtual std::string processor_name() const {
+  //    return "stream_sink";
+  //  }
+
+  //  virtual bool process_one(int64_t tick) {
+  //    size_t count = 0;
+  //    while (this->_queue.size()) {
+  //      auto ev = this->_queue.front();
+  //      handle_event(ev);
+  //      this->_queue.pop_front();
+  //    }
+  //    return (count > 0);
+  //  }
+ 
+  //  virtual size_t queue_len() {
+  //    return this->_queue.size();
+  //  }
+
+  //  virtual void commit(bool flush) {
+  //    // noop
+  //  }
+  //   
+  //private:
+  //  void handle_event(std::shared_ptr<kevent<void, V>> ev) {
+  //    _os << "ts: " << ev->event_time() << "  ";
+  //    _codec->encode(*ev->record()->value, _os);
+  //    _os << std::endl;
+  //  }
+
+  //  std::ostream&                      _os;
+  //  std::shared_ptr<kspp::text_serdes> _codec;
+  //};
+
+  //// <key, nullptr>
+  //template<class K>
+  //class stream_sink<K, void> : public partition_sink<K, void>
+  //{
+  //public:
+  //  stream_sink(topology_base& topology, std::shared_ptr<partition_source<K, void>> source, std::ostream* os)
+  //    : partition_sink<K, void>(source->partition())
+  //    , _os(*os)
+  //    , _codec(std::make_shared<kspp::text_serdes>()) {
+  //    source->add_sink(this);
+  //  }
+
+  //  virtual ~stream_sink() {
+  //    this->flush();;
+  //  }
+
+  //  virtual std::string name() const {
+  //    return "stream_sink";
+  //  }
+
+  //  virtual std::string processor_name() const {
+  //    return "stream_sink";
+  //  }
+
+  //  virtual bool process_one(int64_t tick) {
+  //    size_t count = 0;
+  //    while (this->_queue.size()) {
+  //      auto ev = this->_queue.front();
+  //      handle_event(ev);
+  //      this->_queue.pop_front();
+  //    }
+  //    return (count > 0);
+  //  }
+
+  //  virtual size_t queue_len() {
+  //    return this->_queue.size();
+  //  }
+
+  //  virtual void commit(bool flush) {
+  //    // noop
+  //  }
+
+  //private:
+  //  void handle_event(std::shared_ptr<kevent<K, void>> ev) {
+  //    _os << "ts: " << ev->event_time() << "  ";
+  //    _codec->encode(ev->record()->key, _os);
+  //    _os << std::endl;
+  //  }
+
+  //  std::ostream&                      _os;
+  //  std::shared_ptr<kspp::text_serdes> _codec;
+  //};
 
 };
