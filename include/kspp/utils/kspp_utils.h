c2527e1650bbf4eb06fccbdfd93b5c826db217db
skarlsson <svante.karlsson@csi.se>
2017-09-26 11:15:26 +0200

    adds missing file

---
+++ include/kspp/utils/kspp_utils.h
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+#pragma once
+
+namespace kspp {
+  std::string sanitize_filename(std::string s);
+
+  std::vector<int> parse_partition_list(std::string s);
+
+  std::vector<int> get_partition_list(int32_t nr_of_partitions);
+}
