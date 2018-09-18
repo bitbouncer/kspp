a6dfbdd12dfa9ac0ecf629c322c85b6dfa72e82c
svante <svante.karlsson@csi.se>
2018-09-18 15:24:22 +0200

    temporary removed obsolete windows & ios support

--- rebuild-all-options.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-rm -rf build bin lib
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_ROCKSDB=ON -DBUILD_SAMPLES=ON -DBUILD_TESTS=ON -DBUILD_TOOLS=ON ..
-make -j8 
-cd ..
-
