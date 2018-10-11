b2551e6f1a59361e2f6ed9ea806704ef82bb3744
svante <svante.karlsson@csi.se>
2018-10-11 11:23:42 +0200

    fixes docker build sample

--- docker-ubuntu/build.sh
+++ docker-ubuntu/build.sh
@@ -10,30 +10,31 @@ pushd ..
 docker build -f docker-ubuntu/Dockerfile.build  --no-cache -tkspp-build-ubuntu .
 
 popd
-docker create --name extract kspp-build-ubuntu
-docker cp extract:/usr/local/lib/libavrocpp.so.1.8.2.0       ./extract/lib
-docker cp extract:/usr/local/lib/libbenchmark.so.0.0.0       ./extract/lib
-docker cp extract:/usr/local/lib/libbenchmark_main.so.0.0.0  ./extract/lib
-docker cp extract:/usr/local/lib/libcivetweb-cpp.so.1.11.0   ./extract/lib
-docker cp extract:/usr/local/lib/libcivetweb.so.1.11.0       ./extract/lib
-docker cp extract:/usr/local/lib/libcpr.so                   ./extract/lib
-docker cp extract:/usr/local/lib/libkspp.so                  ./extract/lib
-docker cp extract:/usr/local/lib/libkspp_es.so               ./extract/lib
-docker cp extract:/usr/local/lib/libkspp_influxdb.so         ./extract/lib
-docker cp extract:/usr/local/lib/libkspp_pg.so               ./extract/lib
-docker cp extract:/usr/local/lib/libkspp_rocksdb.so          ./extract/lib
-docker cp extract:/usr/local/lib/libkspp_tds.so              ./extract/lib
-docker cp extract:/usr/local/lib/librdkafka++.so.1           ./extract/lib
-docker cp extract:/usr/local/lib/librdkafka.so.1             ./extract/lib
-docker cp extract:/usr/local/lib/librocksdb.so               ./extract/lib
+docker rm -f extract_ubuntu || true
+docker create --name extract_ubuntu kspp-build-ubuntu
+docker cp extract_ubuntu:/usr/local/lib/libavrocpp.so.1.8.2.0       ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libbenchmark.so.0.0.0       ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libbenchmark_main.so.0.0.0  ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libcivetweb-cpp.so.1.11.0   ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libcivetweb.so.1.11.0       ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libcpr.so                   ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libkspp.so                  ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libkspp_es.so               ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libkspp_influxdb.so         ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libkspp_pg.so               ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libkspp_rocksdb.so          ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libkspp_tds.so              ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/librdkafka++.so.1           ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/librdkafka.so.1             ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/librocksdb.so               ./extract/lib
 
-docker cp extract:/usr/local/lib/libgmock.so               ./extract/lib
-docker cp extract:/usr/local/lib/libgtest.so               ./extract/lib
-docker cp extract:/usr/local/lib/libgmock_main.so          ./extract/lib
-docker cp extract:/usr/local/lib/libgtest_main.so          ./extract/lib
-docker cp extract:/usr/local/lib/libprometheus-cpp-core.so ./extract/lib
-docker cp extract:/usr/local/lib/libprometheus-cpp-pull.so ./extract/lib
-docker cp extract:/usr/local/lib/libprometheus-cpp-push.so ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libgmock.so                 ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libgtest.so                 ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libgmock_main.so            ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libgtest_main.so            ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libprometheus-cpp-core.so   ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libprometheus-cpp-pull.so   ./extract/lib
+docker cp extract_ubuntu:/usr/local/lib/libprometheus-cpp-push.so   ./extract/lib
 
 #docker cp extract:/usr/local/lib64/libgmock.so               ./extract/lib64
 #docker cp extract:/usr/local/lib64/libgtest.so               ./extract/lib64
@@ -43,15 +44,15 @@ docker cp extract:/usr/local/lib/libprometheus-cpp-push.so ./extract/lib
 #docker cp extract:/usr/local/lib64/libprometheus-cpp-pull.so ./extract/lib64
 #docker cp extract:/usr/local/lib64/libprometheus-cpp-push.so ./extract/lib64
 
-docker cp extract:/usr/local/bin/kafka2es                    ./extract/bin
-docker cp extract:/usr/local/bin/kafka2influxdb              ./extract/bin
-docker cp extract:/usr/local/bin/kafka2postgres              ./extract/bin
-docker cp extract:/usr/local/bin/postgres2kafka              ./extract/bin
-docker cp extract:/usr/local/bin/tds2kafka                   ./extract/bin
+docker cp extract_ubuntu:/usr/local/bin/kafka2es                    ./extract/bin
+docker cp extract_ubuntu:/usr/local/bin/kafka2influxdb              ./extract/bin
+docker cp extract_ubuntu:/usr/local/bin/kafka2postgres              ./extract/bin
+docker cp extract_ubuntu:/usr/local/bin/postgres2kafka              ./extract/bin
+docker cp extract_ubuntu:/usr/local/bin/tds2kafka                   ./extract/bin
 
-docker cp extract:/src/runDeps                               ./extract/runDeps
+docker cp extract_ubuntu:/src/runDeps                               ./extract/runDeps
 
-docker rm -f extract
+docker rm -f extract_ubuntu
 
 docker build -f Dockerfile --no-cache -tkspp-sample-ubuntu .
 
