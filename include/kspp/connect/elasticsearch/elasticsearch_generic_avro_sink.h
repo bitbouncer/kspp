73ee85b303723ebebee9a229d161f025320d83fd
svante <svante.karlsson@csi.se>
2019-02-24 18:05:05 +0100

    add max_http-connection as parameter

--- include/kspp/connect/elasticsearch/elasticsearch_generic_avro_sink.h
+++ include/kspp/connect/elasticsearch/elasticsearch_generic_avro_sink.h
@@ -6,8 +6,8 @@ namespace kspp {
   class elasticsearch_generic_avro_sink : public generic_avro_sink {
     static constexpr const char *PROCESSOR_NAME = "elasticsearch_avro_sink";
   public:
-    elasticsearch_generic_avro_sink(std::shared_ptr<cluster_config> config, const kspp::connect::connection_params& cp)
-        : generic_avro_sink(config, std::make_shared<kspp::elasticsearch_producer>(cp, 100)) {
+    elasticsearch_generic_avro_sink(std::shared_ptr<cluster_config> config, const kspp::connect::connection_params& cp, size_t max_http_connection=20)
+        : generic_avro_sink(config, std::make_shared<kspp::elasticsearch_producer>(cp, max_http_connection)) {
       this->add_metrics_tag(KSPP_PROCESSOR_TYPE_TAG, PROCESSOR_NAME);
       this->add_metrics_tag(KSPP_TOPIC_TAG, cp.database_name);
 
