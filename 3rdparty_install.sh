78682db4ffd9aa77428b71d0d8b05c6207f7b090
svante <svante.karlsson@csi.se>
2019-06-28 17:13:04 +0200

    new cmake flag

--- 3rdparty_install.sh
+++ 3rdparty_install.sh
@@ -45,7 +45,7 @@ cat avro/lang/c++/CMakeLists.txt
 cd avro/lang/c++/ 
 mkdir build 
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release .. -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_STANDARD=$CPP_STANDARD
+cmake -DCMAKE_BUILD_TYPE=Release .. -DBUILD_SHARED_LIBS=ON -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_CXX_STANDARD=$CPP_STANDARD
 make -j "$(getconf _NPROCESSORS_ONLN)"
 sudo make install
 cd ../../../..
