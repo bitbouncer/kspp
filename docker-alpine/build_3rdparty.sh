e9f6cf9eda5659ea4b116eca8120638633684b99
svante <svante.karlsson@csi.se>
2019-08-03 17:14:21 +0200

    removes nocache

--- docker-alpine/build_3rdparty.sh
+++ docker-alpine/build_3rdparty.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+IMAGE_TAG=${1:-latest}
+
 pushd ..
-docker build -f docker-alpine/Dockerfile.build3rdparty --no-cache -tkspp-build3rdparty-alpine .
+docker build -f docker-alpine/Dockerfile.build3rdparty -tkspp-build3rdparty-alpine:${IMAGE_TAG} .
 popd
 
 
