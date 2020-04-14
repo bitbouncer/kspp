f6de78b062c4b76e6267a7efffce624f9354e361
svante karlsson <svante.karlsson@csi.se>
2020-04-14 22:08:38 +0200

    refactors CMakeFile to only build one kspp lib + grpc lib

--- install.sh
+++ install.sh
@@ -3,9 +3,7 @@ rm -rf build bin lib
 mkdir build
 cd build
 sudo rm -rf /usr/local/include/kspp
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_STATIC_LIBS=OFF -DBUILD_SHARED_LIBS=ON ..
 make -j "$(getconf _NPROCESSORS_ONLN)"
 sudo make install
-
-
 
