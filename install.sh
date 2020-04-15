fe6bb18090bcae293629353e21537ca3d35c3b5f
svante karlsson <svante.karlsson@csi.se>
2020-04-15 10:28:22 +0200

    fixes install script

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+set -e 
+
 rm -rf build bin lib
 mkdir build
 cd build
 sudo rm -rf /usr/local/include/kspp
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_STATIC_LIBS=OFF -DBUILD_SHARED_LIBS=ON ..
+sudo rm -rf /usr/local/lib/libkspp*.so
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_STATIC_LIBS=OFF -DBUILD_SHARED_LIBS=ON -DLINK_SHARED=ON -DBUILD_TESTS=OFF -DBUILD_SAMPLES=OFF ..
 make -j "$(getconf _NPROCESSORS_ONLN)"
 sudo make install
 
