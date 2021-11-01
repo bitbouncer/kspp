e2c260013551c8be7427dc91048c2a7667eefeb3
Toshiya TSURU <toshiya.tsuru@d8s.co.jp>
2021-11-01 17:02:41 +0900

    Fix for avro release-1.11.0. https://github.com/apache/avro/releases/tag/release-1.11.0

--- src/connect/postgres/postgres_avro_utils.cpp
+++ src/connect/postgres/postgres_avro_utils.cpp
@@ -245,7 +245,8 @@ namespace kspp {
       auto r = schema->root();
       assert(r->type() == avro::AVRO_RECORD);
       if (r->hasName()) {
-        std::string name = r->name();
+        auto n = r->name();
+        std::string name = n.fullname();
 
         //since we use convention tablename.key / table_name.value (until we know what bottledwater does...)
         // return namesapace as table name
@@ -254,7 +255,7 @@ namespace kspp {
           std::string ns = name.substr(0, found);
           return ns;
         }
-        return r->name();
+        return n.fullname();
       }
       assert(false);
       return "unknown_table_name";
